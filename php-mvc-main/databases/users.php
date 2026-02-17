15058055866fd4b993e9c6ca78c42deebfa29221
KillerKung <132036599+KillerKung@users.noreply.github.com>
2026-02-17 19:57:27 +0700

    Add files via upload

--- php-mvc-main/databases/users.php
+++ php-mvc-main/databases/users.php
@@ -12,3 +12,18 @@ function insertUsers($user): bool
         return false;
     }
 }
+
+function checkLogin(string $email, string $password): bool
+{
+    global $conn;
+    $sql = 'select password from users where email = ?';
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result && $result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return password_verify($password, $row['password']);
+    }
+    return false;
+}

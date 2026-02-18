c49502df45301cef705710e39d8c62974ea22f5b
KillerKung <132036599+KillerKung@users.noreply.github.com>
2026-02-18 09:11:46 +0700

    Add files via upload

--- php-mvc-main/routes/createAcc.php
+++ php-mvc-main/routes/createAcc.php
@@ -1,12 +1,21 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $username = $_POST['Uname'] ?? '';
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $password  = $_POST['password'] ?? '';
     $Cpassword = $_POST['Cpassword'] ?? '';
-    if($Cpassword != $password){
-        
+    if ($password !== $Cpassword) {
+        renderView('createAcc', ['error' => 'รหัสผ่านไม่ตรงกัน']);
+        exit;
     }
+    $user = [
+        'name'     => $_POST['Uname'] ?? '',
+        'email'    => $_POST['email'] ?? '',
+        'password' => $password,
+    ];
+    header('Location: /home');   
+    exit;
+
+} else {
+    renderView('createAcc', ['error' => null]);
 }
-renderView('createAcc');

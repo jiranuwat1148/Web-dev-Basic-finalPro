fbff410d844b6a0e65525d8a0d35d5d0ccac4f1c
๋Jiranuwat2081 <film.phumdonchai@gmail.com>
2026-03-11 02:30:39 +0700

    Implement OTP check-in logic in checkin-even.php
    Handle OTP input and render appropriate views based on the check-in status.

--- php-mvc-main/routes/checkin-even.php
+++ php-mvc-main/routes/checkin-even.php
@@ -1,2 +1,29 @@
 <?php
-renderView('checkin-even');
+require_once DATABASES_DIR . '/otp.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userInput = $_POST['otp_input'] ?? '';
+    $status = otpindatabse($userInput); 
+    
+    if ($status === 'checkin success') {
+        renderView('checkin-even', [
+            'title' => 'checkin result',
+            'message' => 'you are check in success'
+        ]);
+    } elseif ($status === 'otp is expire') {
+        renderView('checkin-even', [
+            'title' => 'otp is expired',
+            'message' => 'sorry your otp is expire'
+        ]);
+    } else {
+        renderView('checkin-even', [
+            'title' => 'invalic OTP',
+            'message' => 'turn back to home page and call support or try reconnect'
+        ]);
+    }
+} else {
+    renderView('checkin-even', [
+        'title' => 'Check-in OTP',
+        'message' => 'Please enter your OTP code.'
+    ]);
+}

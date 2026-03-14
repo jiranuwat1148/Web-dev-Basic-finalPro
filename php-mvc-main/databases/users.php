5cb834513f9088679d05e7103b6c19b88bb8400f
KillerKung <132036599+KillerKung@users.noreply.github.com>
2026-03-14 15:03:27 +0700

    Update การลงทะเบียน

--- php-mvc-main/databases/users.php
+++ php-mvc-main/databases/users.php
@@ -4,9 +4,9 @@
 function insertUsers($user): bool
 {
     global $conn;
-    $sql = 'insert into users (name, email, password) VALUES (?,?,?)';
+    $sql = 'insert into users (name, email, password, gender, birthday) VALUES (?,?,?,?,?)';
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param('sss', $user['name'], $user['email'], $user['password']);
+    $stmt->bind_param('sssss', $user['name'], $user['email'], $user['password'], $user['gender'], $user['birthday']);
     $stmt->execute();
     if ($stmt->affected_rows > 0) {
         return true;
